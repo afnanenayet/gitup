a55de2a932d092e707270b5c3d3098e8b61f231e
Afnan Enayet <afnan.enayet@gmail.com>
2018-09-21 06:36:15 -0400

    Update dependency versions and new crates
    - update dependencies
    - use error-chain crate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+#![recursion_limit = "1024"]
+
 #[macro_use]
 extern crate log;
 extern crate env_logger;
 extern crate git2;
+#[macro_use]
+extern crate error_chain;
 
 mod consts;
 mod git;
@@ -12,7 +16,22 @@ use git::update_repo;
 use std::collections::HashMap;
 use std::path::PathBuf;
 
-fn main() {
+// Import the macro. Don't forget to add `error-chain` in your
+// `Cargo.toml`!
+
+// We'll put our errors in an `errors` module, and other modules in
+// this crate will `use errors::*;` to get access to everything
+// `error_chain!` creates.
+mod errors {
+    // Create the Error, ErrorKind, ResultExt, and Result types
+    error_chain!{}
+}
+
+pub use errors::*;
+
+quick_main!(run);
+
+fn run() -> Result<()>{
     env_logger::init();
 
     let path = PathBuf::from(".");
@@ -20,4 +39,5 @@ fn main() {
     branches.insert(String::from("master"), true);
     println!("Updating repo at {:#?}", path);
     update_repo(&path, "origin", &branches).unwrap();
+    Ok(())
 }
